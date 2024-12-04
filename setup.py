0c37bae4b6b58dfe6a890a2c5dae23b0191485fe
mateo <mcardonaserrano@berkeley.edu>
2024-12-04 00:03:26 -0800

    Implemented vectorized arrays to enable batch predictions for efficient modeling.

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="MISA_XGModel",  # Replace with your project name
-    version="0.2.0",  # Semantic versioning (Major.Minor.Patch)
-    author="Mateo Cardona Serrano",  # Replace with your name
-    author_email="mcardonaserrano@berkeley.edu",  # Replace with your email
-    description="A Python library for predicting electron density using XGBoost",  # Short description
+    name="MISA_XGModel",
+    version="0.2.1",  # Semantic versioning (Major.Minor.Patch)
+    author="Mateo Cardona Serrano",
+    author_email="mcardonaserrano@berkeley.edu",
+    description="A Python library for predicting electron density using XGBoost",
     long_description=long_description,  # Use the content of README.md
-    long_description_content_type="text/markdown",  # Specify the content type
-    url="https://github.com/mcardonaserrano/MISA_XGModel",  # Replace with your GitHub URL
-    packages=find_packages(),  # Automatically find all packages
+    long_description_content_type="text/markdown",
+    url="https://github.com/mcardonaserrano/MISA_XGModel",
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
